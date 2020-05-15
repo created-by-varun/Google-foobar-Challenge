6331dab77606a7cb0682b7826fe64d73cd65f0c7
Varun V <56495025+created-by-varun@users.noreply.github.com>
2020-05-15 10:12:01 +0530

    Update solution.py

--- level2/solution.py
+++ level2/solution.py
@@ -1,22 +1,19 @@
 # Note: this solution is failing 2 test cases at the moment
 
 def solution(total_lambs):
-    return stingy(total_lambs) - generous(total_lambs)
+    totals = [generous(total_lambs), stingy(total_lambs)]
+    difference = max(totals) - min(totals)
+    return difference
 
 def generous(total_lambs):
     num = 1
-    last = 0
-    cur = 1
-    total_lambs -= 1
-    while total_lambs > 0:
-        if total_lambs < cur * 2:
-            if total_lambs >= cur + last:
-                num += 1
+    while True:
+        total = 2**(num) - 1
+        if total <= total_lambs:
+            num += 1
+        else:
+            num -= 1
             break
-        num += 1
-        cur, last = cur * 2, cur
-        total_lambs -= cur
-
     return num
 
 def stingy(total_lambs):
